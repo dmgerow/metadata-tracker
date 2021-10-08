d0b924cd1a002470e9add9ea2b95413534bcc572
Dylan Gerow <dgerow@salesforce.com>
2021-10-08 13:53:44 -0500

    remove old issue classes

--- force-app/main/default/aura/feedback/feedbackController.js
+++ force-app/main/default/aura/feedback/feedbackController.js
@@ -1,15 +1,5 @@
 ({
-    doSubmitFeedback: function (component, event, helper) {
-        helper.doSubmitFeedback(component);
-    },
-    doCloseFeedback: function (component, event, helper) {
-        helper.doCloseFeedback(component);
-    },
-    doValidateBody: function (component, event, helper) {
-        if (component.get("v.body")) {
-            component.set("v.buttonDisabled",false);
-        } else {
-            component.set("v.buttonDisabled", true);
-        }
-    }
-})
+  doCloseFeedback: function (component, event, helper) {
+    helper.doCloseFeedback(component);
+  }
+});
--- force-app/main/default/aura/feedback/feedbackHelper.js
+++ force-app/main/default/aura/feedback/feedbackHelper.js
@@ -1,46 +1,6 @@
 ({
-    doSubmitFeedback: function (component) {
-        component.set("v.buttonDisabled", true);
-        var action = component.get("c.sendFeedback");
-        var requestModel = {
-            "body": component.get("v.body")
-        };
-        var requestString = JSON.stringify(requestModel);
-        console.log(requestString);
-        action.setParams({
-            "request": requestString
-        });
-        action.setCallback(this, function (response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                this.doFireToastEvent('Feedback submitted', 'success');
-                this.doCloseFeedback(component);
-            } else if (state === "ERROR") {
-                var errors = response.getError();
-                if (errors) {
-                    if (errors[0] && errors[0].message) {
-                        console.log(errors[0].message);
-                        this.doFireToastEvent(errors[0].message, 'error');
-                    }
-                } else {
-                    console.log("Unknown error");
-                }
-            }
-            component.set("v.buttonDisabled", false);
-        });
-        $A.enqueueAction(action);
-    },
-    doFireToastEvent: function (message, type) {
-        var toastEvent = $A.get("e.force:showToast");
-        toastEvent.setParams({
-            type: type,
-            mode: 'dismissible',
-            message: message
-        });
-        toastEvent.fire();
-    },
-    doCloseFeedback: function (component) {
-        var closeEvent = component.getEvent("closeFeedback");
-        closeEvent.fire();
-    }
-})
+  doCloseFeedback: function (component) {
+    var closeEvent = component.getEvent("closeFeedback");
+    closeEvent.fire();
+  }
+});
